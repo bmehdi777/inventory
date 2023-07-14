6d9b671a2e8f02aa2affd34266aa6af7a0c217c0
Bentouati Mehdi <mehdi.bentouati76@gmail.com>
2023-07-14 21:25:15 +0200

    Init commit

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use axum::{
+    routing::{get},
+    http::StatusCode, 
+    Router,
+};
+use log::info;
+
+pub async fn run() {
+    let app = Router::new().route("/healthcheck", get(healthcheck));
+    axum::Server::bind(&"127.0.0.1:8000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn healthcheck() -> StatusCode {
+    info!("/healthcheck is alive");
+    StatusCode::OK
+}
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+#[tokio::main]
+async fn main() {
+    inventory_api::run().await;
+}
