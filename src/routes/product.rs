3f6bac7bf43e131b892dbf7578698fcc2bb4f4e2
Bentouati Mehdi <mehdi.bentouati76@gmail.com>
2023-07-23 16:02:18 +0200

    WIP - passing connection through db_client

--- src/routes/product.rs
+++ src/routes/product.rs
@@ -1,6 +1,20 @@
-use axum::{response::Json};
+use axum::{response::Json, http::StatusCode, extract::State};
 use serde_json::{Value, json};
+use mongodb::Database;
 
-pub async fn get_products() -> Json<Value> {
-    Json(json!({"title": "test"}))
+pub async fn get_products(State(db_client): State<Database>) -> Result<Json<Value>, StatusCode> {
+    let list = db_client.list_collection_names(None).await;
+    match list {
+        Ok(l) => {
+            for collections in l {
+                log::info!("Collections available : {}", collections);
+            }
+            return Ok(Json(json!({"title": "test"})));
+        },
+        Err(e) => {
+            log::error!("{}", e);
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+
+    }
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,34 +1,35 @@
 use crate::{configuration::Settings, health_check::health_check, product};
 use axum::{routing::get, Router};
-use mongodb::{Client, options::ClientOptions};
+use mongodb::{options::ClientOptions, Client, Database};
 
 pub async fn run(configuration: Settings) -> anyhow::Result<()> {
     log::info!("Server is listening on http://127.0.0.1:8000");
 
-    let mut options = ClientOptions::parse(configuration.database.connection_string()).await?;
-    options.connect_timeout = Some(std::time::Duration::new(1, 0));
-    options.direct_connection = Some(true);
+    let db_client = set_db(&configuration).await?;
 
-    log::info!("options : {:?}", options);
+    let app = Router::new()
+        .route("/healthcheck", get(health_check))
+        .route("/products", get(product::get_products))
+        .with_state(db_client.clone());
 
-    let client = Client::with_options(options)?.database("inventory");
-
-    for collections in client.list_collection_names(None).await? {
-        log::info!("Collections available : {}", collections);
-    }
-
-    //let app = Router::new()
-    //    .route("/healthcheck", get(health_check))
-    //    .route("/products", get(product::get_products));
-    //axum::Server::bind(
-    //    &format!("127.0.0.1:{}", configuration.application_port)
-    //        .parse()
-    //        .unwrap(),
-    //)
-    //.serve(app.into_make_service())
-    //.await
-    //.unwrap();
+    axum::Server::bind(
+        &format!("127.0.0.1:{}", configuration.application_port)
+            .parse()
+            .unwrap(),
+    )
+    .serve(app.into_make_service())
+    .await
+    .unwrap();
 
     Ok(())
 }
 
+pub async fn set_db(configuration: &Settings ) -> mongodb::error::Result<Database>  {
+    let mut options = ClientOptions::parse(configuration.database.connection_string()).await?;
+    options.connect_timeout = Some(std::time::Duration::new(5,0));
+    options.direct_connection = Some(true);
+
+    log::debug!("Connecting to database with the following options : {:?}", options);
+
+    Ok(Client::with_options(options)?.database("inventory"))
+}
