60f84e2d37799a16c6ab6d36d386947d38838782
Bentouati Mehdi <mehdi.bentouati76@gmail.com>
2023-08-03 23:28:36 +0200

    Add tracing json format

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
-use inventory_api::{startup, configuration};
+use inventory_api::{configuration, startup};
+use tracing_bunyan_formatter::BunyanFormattingLayer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter};
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt().init();
+    set_tracing();
+
     let configuration = configuration::get_configuration().expect("Failed to read env file");
 
     startup::run(configuration).await.unwrap();
+}
+
+fn set_tracing() {
+    let formated = BunyanFormattingLayer::new("Inventory_API".into(), std::io::stdout);
+    let registry = tracing_subscriber::registry::Registry::default()
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")))
+        //.with(tracing_subscriber::fmt::layer().compact());
+        .with(formated);
+
+    tracing::subscriber::set_global_default(registry).expect("Failed to set subscriber.");
 }
--- src/routes/product/get.rs
+++ src/routes/product/get.rs
@@ -6,7 +6,9 @@ use axum::{extract::State, response::Json};
 use futures::TryStreamExt;
 use serde_json::{json, Value};
 
+#[tracing::instrument(name = "GET products", skip(db_client))]
 pub async fn get_products(State(db_client): State<DatabaseRC>) -> Result<Json<Value>, AppError> {
+    tracing::info!("Querying list of product");
     let products: Vec<Product> = db_client
         .collection::<Product>(PRODUCT_TABLENAME)
         .find(None, None)
@@ -14,8 +16,7 @@ pub async fn get_products(State(db_client): State<DatabaseRC>) -> Result<Json<Va
         .try_collect()
         .await?;
 
-    tracing::debug!("{:?}", products);
-
+    tracing::info!("Sending list of product");
     Ok(Json(json!(products)))
 }
 
